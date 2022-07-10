edbedef209c91ce32f302a95229147a059fdfe29
BLACK SHADOW <87250241+Mehadi-Hasan-Shanto@users.noreply.github.com>
2022-07-10 17:50:49 +0600

    Add files via upload

---
+++ eid.py
@@ -0,0 +1,166 @@
+#MD Shanto Islam
+#BLACK SHADOW
+
+import turtle
+import random
+import turtle
+wn=turtle.Screen()
+turtle.bgcolor('black')
+turtle.shape('turtle')
+import time
+
+time.sleep(5)
+tr=turtle.Turtle()
+ts=turtle.Turtle()
+tt=turtle.Turtle()
+t2=turtle.Turtle()
+t3=turtle.Turtle()
+t4=turtle.Turtle()
+t5=turtle.Turtle()
+move=1
+###############################
+t5.speed('fastest')
+for i in range(10):
+    for i in range(4):
+          t5.pu()
+          t5.goto(500,200)
+          t5.pd()
+          t5.color('cyan')
+          t5.pensize(3)
+          t5.circle(50,steps=4)
+          t5.right(100)
+  
+# creating turtle object
+t = turtle.Turtle()
+turtle.left(520)
+  
+# to activate turtle graphics Screen
+w = turtle.Screen()
+  
+# setting speed of turtle
+turtle.speed(3)
+  
+# giving the background color of turtle
+# graphics screen
+w.bgcolor("black")
+  
+# giving the color of pen to our turtle
+# for drawing
+t.color("white")
+  
+# giving title to our turtle graphics window
+w.title("Ramadan Mubarak")
+
+
+# for making our moon tooking up the pen
+t.up()
+  
+# going at the specific coordinated
+t.goto(0, -300)
+  
+# took down the pen to start drawing
+t.down()
+  
+# giving color to turtle's pen
+t.color("white")
+  
+# start filling the color
+t.begin_fill()
+  
+# making our moon
+t.circle(200)
+  
+# stop filling the color
+t.end_fill()
+  
+# after drawing hidding the turtle from
+# the window
+t.hideturtle()
+  
+# terminated the window after clicking
+#w.exitonclick()
+ 
+#window.exitonclick()
+
+
+
+# for making our moon tooking up the pen
+t.up()
+  
+# going at the specific coordinated
+t.goto(50, -300)
+  
+# took down the pen to start drawing
+t.down()
+  
+# giving color to turtle's pen
+t.color("black")
+  
+# start filling the color
+t.begin_fill()
+  
+# making our moon
+t.circle(200)
+  
+# stop filling the color
+t.end_fill()
+  
+# after drawing hidding the turtle from
+# the window
+t.hideturtle()
+  
+# terminated the window after clicking
+#w.exitonclick()
+ 
+
+
+import turtle as t
+
+t.speed(10000)
+angle=150
+side=90
+pointies = 5
+ROTATION = 360/pointies
+angle_left=angle
+angle_right=angle
+
+t.color("gray","ghost white")
+
+for r in range(10):
+
+ t.begin_fill()
+ for p in range(pointies):
+
+  t.forward(side)
+  t.right(angle_right)
+  t.forward(side)
+  t.left(angle_left)
+  t.right(ROTATION)
+
+ t.end_fill()
+
+ t.right(2)
+
+ #turtle.hideturtle()
+turtle.penup ()
+turtle.goto (0, -200)
+turtle.pendown()
+turtle.color ('white')
+turtle.right(100)
+turtle.write  ('Eid al-Adha TC 2022',font= ('Verdana',19,"bold"))
+#turtle.done()
+
+
+############################
+t5.speed("fastest")
+for i in range(10):
+    for i in range(4):
+          t5.pu()
+          t5.goto(600,0)
+          t5.pd()
+          t5.color('green')
+          t5.pensize(1)
+          t5.circle(20,steps=9)
+          t5.right(100)
+
+turtle.done()
